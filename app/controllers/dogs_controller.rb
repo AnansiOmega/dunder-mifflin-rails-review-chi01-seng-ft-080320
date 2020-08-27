da4516388ebe324a1c446b3aff091ee29e4140ab
AnansiOmega <ignasbutautas7@gmail.com>
2020-08-27 16:31:42 -0500

    finished in time

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,37 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.find(params[:id])
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def edit
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def dog_params
+
+    end
+
 end
